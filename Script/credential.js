a9d5ad8865a5d41bf9968768ada459b005a50b13
HLRobotics <akhil.pj007@gmail.com>
2019-09-08 12:15:03 +0530

    updated

--- Script/credential.js
+++ Script/credential.js
@@ -2,8 +2,8 @@ function engine()
 {
     developer = document.getElementById('dev').value;
     key = document.getElementById('key').value;
-    data_dev="q";
-    data_key="q";
+    data_dev="HLEngine_Bangalore";
+    data_key="rootuser";
     if(developer==data_dev && key== data_key)
     {
     alert("By clicking OK, You are here with agree that HLEngine will be used only under the development activities for HLRobotics.");
